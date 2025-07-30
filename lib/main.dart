c201cb85a3e530604612e98c016964ff889fdffa
Hussein Rizk <husseinrizk117@gmail.com>
2025-07-30 09:28:08 +0300

    Update app icon and launcher label

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,6 @@ class FruitsHub extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return MaterialApp(title: 'Fruits Hub');
   }
 }
