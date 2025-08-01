95963e1d81db2a5760678e9f95aa9be3ecd00bc8
Hussein Rizk <husseinrizk117@gmail.com>
2025-08-01 18:01:54 +0300

    finish onboarding ui

--- lib/feature/onboarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/feature/onboarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,7 +1,10 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/utils/widgets/custom_button.dart';
 import 'package:fruits_hub/feature/onboarding/presentation/views/widgets/on_boarding_page_view.dart';
+
+import '../../../../../constants.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
@@ -18,6 +21,18 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
           ),
         ),
+        SizedBox(height: 29),
+
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kHorizintalPadding,
+          ),
+          child: CustomButton(
+            onPressed: () {},
+            text: 'ابدأ الان',
+          ),
+        ),
+        SizedBox(height: 43),
       ],
     );
   }
