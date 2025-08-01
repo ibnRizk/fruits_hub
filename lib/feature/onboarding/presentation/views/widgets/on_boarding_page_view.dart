0d4de7393ccf0a67796be469dcbc5f0ad9670ce5
Hussein Rizk <husseinrizk117@gmail.com>
2025-08-01 16:34:55 +0300

    page view item part one

--- lib/feature/onboarding/presentation/views/widgets/on_boarding_page_view.dart
+++ lib/feature/onboarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+
+import 'page_view_item.dart';
 
 class OnBoardingPageView extends StatelessWidget {
   const OnBoardingPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PageView();
+    return PageView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        PageViewItem(
+          image: Assets.imagesPageViewItem1Image,
+          backgroundImage:
+              Assets.imagesPageViewItem1BackgroundImage,
+          subtitle:
+              'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
+          title: Row(
+            children: [
+              Text('مرحبًا بك في'),
+              Text('Fruit'),
+              Text('HUB'),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
