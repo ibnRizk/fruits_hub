93d1461d09a8b8ffedea72e457d822e981576756
Hussein Rizk <husseinrizk117@gmail.com>
2025-08-02 17:25:26 +0300

    create failure class & auth repo & entity

---
+++ lib/feature/auth/domin/repo/auth_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/failures.dart';
+import 'package:fruits_hub/feature/auth/domin/entites/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>>
+  createuserwithemailandpassword(
+    String email,
+    String password,
+  );
+}
