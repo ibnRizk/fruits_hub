ffc1d338f840e0b8ddcc936f80c46a9782fffe9e
Hussein Rizk <husseinrizk117@gmail.com>
2025-08-02 17:53:11 +0300

    create Firebase Auth Service & user model ....

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_hub/core/errors/exceptions.dart';
+
+class FirebaseAuthService {
+  Future<User> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: email,
+            password: password,
+          );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException(
+          message: 'الرقم السري ضعيف جداً.',
+        );
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(
+          message:
+              'لقد قمت بالتسجيل مسبقاً. الرجاء تسجيل الدخول.',
+        );
+      } else {
+        throw CustomException(
+          message:
+              'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+        );
+      }
+    } catch (e) {
+      throw CustomException(
+        message:
+            'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+      );
+    }
+  }
+}
---
+++ lib/feature/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/failures.dart';
+import 'package:fruits_hub/core/services/firebase_auth_service.dart';
+import 'package:fruits_hub/feature/auth/data/models/user_model.dart';
+import 'package:fruits_hub/feature/auth/domin/entites/user_entity.dart';
+import 'package:fruits_hub/feature/auth/domin/repo/auth_repo.dart';
+
+import '../../../../core/errors/exceptions.dart';
+
+class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+
+  AuthRepoImpl({required this.firebaseAuthService});
+  @override
+  Future<Either<Failure, UserEntity>>
+  createuserwithemailandpassword(
+    String email,
+    String password,
+    String name,
+  ) async {
+    try {
+      var user = await firebaseAuthService
+          .createUserWithEmailAndPassword(
+            email: email,
+            password: password,
+          );
+      return right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      return left(ServerFailure(e.message));
+    } catch (e) {
+      return left(
+        ServerFailure(
+          'حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+        ),
+      );
+    }
+  }
+}
