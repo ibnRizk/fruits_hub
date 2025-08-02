acfdd540eb8b93d82e187769e23072402b0e5a0e
Hussein Rizk <husseinrizk117@gmail.com>
2025-08-02 03:58:30 +0300

    add firebase to project

--- lib/feature/auth/presentation/views/login_view.dart
+++ lib/feature/auth/presentation/views/login_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/feature/auth/presentation/views/widgets/login_view_body.dart';
 
 import '../../../../core/utils/widgets/custom_app_bar.dart';
--- lib/feature/auth/presentation/views/widgets/login_view_body.dart
+++ lib/feature/auth/presentation/views/widgets/login_view_body.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/widgets/custom_button.dart';
